b160ee57de1dfe7d567d048757513f9e2976be17
이유찬 <yuchan.lee@mirero.co.kr>
2022-09-20 15:58:25 +0900

    Study_2022-09-20

--- BoxingUnboxing/MainApp.cs
+++ BoxingUnboxing/MainApp.cs
@@ -7,8 +7,8 @@ namespace BoxingUnboxing
         static void Main(string[] args)
         {
             int a = 123;
-            object b = (object)a;
-            int c = (int)b;
+            object b = (object)a; // a에 담긴 값을 박싱해서 힙에 저장
+            int c = (int)b; // b에 담긴 값을 언박싱해서 스택에 저장
 
             Console.WriteLine(a);
             Console.WriteLine(b);
--- MainApp.cs
+++ MainApp.cs
@@ -6,7 +6,7 @@ namespace Hello
     class MainApp{
         static void Main(string[] args){
             if(args.Length == 0){
-                Console.WriteLine("사용법 : Hello.exe<이름>");
+                WriteLine("사용법 : Hello.exe<이름>");
                 return;
             }
             WriteLine("Hello, {0}!", args[0]);
